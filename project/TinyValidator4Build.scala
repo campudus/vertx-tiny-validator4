3784df2ee864b050d287ffa297edbbd2e863aae4
Alexander Vetter <vettera@gmail.com>
2015-04-28 16:07:53 +0200

    add pomExtra to Build.scala

--- project/TinyValidator4Build.scala
+++ project/TinyValidator4Build.scala
@@ -8,7 +8,7 @@ object TinyValidator4Build extends VertxProject {
     "com.campudus",
     "vertx-tiny-validator4",
     "1.0.0",
-    "tables into sheets",
+    "Vert.x module to validate JSON against a json-schema draft v4.",
     Some("2.11.6"),
     Seq("2.10.5", "2.11.6"))
     
@@ -19,6 +19,28 @@ object TinyValidator4Build extends VertxProject {
       "io.vertx" % "testtools" % "2.0.3-final" % "test",
       "org.hamcrest" % "hamcrest-library" % "1.3" % "test",
       "com.novocode" % "junit-interface" % "0.11" % "test"
-    )
+    ),
+
+    pomExtra :=
+      <inceptionYear>2015</inceptionYear>
+      <url>https://github.com/campudus/vertx-tiny-validator4</url>
+      <licenses>
+        <license>
+          <name>Apache License, Version 2.0</name>
+          <url>http://www.apache.org/licenses/LICENSE-2.0.html</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <connection>scm:git:https://github.com/campudus/vertx-tiny-validator4.git</connection>
+        <developerConnection>scm:git:ssh://git@github.com:campudus/vertx-tiny-validator4.git</developerConnection>
+        <url>https://github.com/campudus/vertx-tiny-validator4</url>
+      </scm>
+      <developers>
+        <developer>
+          <id>alexvetter</id>
+          <name>Alexander Vetter</name>
+        </developer>
+      </developers>
   )
 }
